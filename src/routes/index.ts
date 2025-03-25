b8e4e6671805229254614ee2d6734f2f8131cfa2
Vaibhav G <vaibhavvishnu456@gmail.com>
2025-03-25 10:14:47 +0530

    feat: add login functionality with JWT token generation and error handling

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,84 @@
 import { Hono } from "hono";
-import { signUpWithUsernameAndPassword } from "../controllers/authentication";
-import { SignUpWithUsernameAndPasswordError } from "../controllers/authentication/+type";
+import {
+  logInWithUsernameAndPassword,
+  signUpWithUsernameAndPassword,
+} from "../controllers/authentication";
+import {
+  LogInWtihUsernameAndPasswordError,
+  SignUpWithUsernameAndPasswordError,
+} from "../controllers/authentication/+type";
 
 export const hono = new Hono();
+
+hono.post("/authentication/sign-up", async (context) => {
+  const { username, password } = await context.req.json();
+
+  try {
+    const result = await signUpWithUsernameAndPassword({
+      username,
+      password,
+    });
+
+    return context.json(
+      {
+        data: result,
+      },
+      201
+    );
+  } catch (e) {
+    if (e === SignUpWithUsernameAndPasswordError.CONFLICTING_USERNAME) {
+      return context.json(
+        {
+          message: "Username already exists",
+        },
+        409
+      );
+    }
+
+    return context.json(
+      {
+        mesage: "Unknown",
+      },
+      500
+    );
+  }
+});
+
+hono.post("/authentication/log-in", async (context) => {
+  try {
+    const { username, password } = await context.req.json();
+
+    const result = await logInWithUsernameAndPassword({
+      username,
+      password,
+    });
+
+    return context.json(
+      {
+        data: result,
+      },
+      201
+    );
+  } catch (e) {
+    if (
+      e === LogInWtihUsernameAndPasswordError.INCORRECT_USERNAME_OR_PASSWORD
+    ) {
+      return context.json(
+        {
+          message: "Incorrect username or password",
+        },
+        401
+      );
+    }
+
+    return context.json(
+      {
+        message: "Unknown",
+      },
+      500
+    );
+  }
+});
 
 hono.get("/health", (context) => {
   return context.json(
@@ -12,42 +88,3 @@ hono.get("/health", (context) => {
     200
   );
 });
-
-hono.post("/authentications/sign-up", async (context) => {
-    const {username, password} = await context.req.json();
-
-    try{
-        const result = await signUpWithUsernameAndPassword({username, password});
-        return context.json({
-            data: result,
-        },
-        201);
-
-
-    }catch (e){
-        if(e === SignUpWithUsernameAndPasswordError.CONFLICTING_USERNAME){
-            return context.json(
-                {
-                    message: "Username already exists",
-                },
-                409
-            );
-        }
-
-        if(e === SignUpWithUsernameAndPasswordError.UNKNOWN){
-            return context.json(
-                {
-                    message: "Unknown error",
-                },
-                500
-            );
-        }
-            return context.json(
-              {
-                message: "Unknown error",
-              },
-              500
-            );
-    }
-}
-);
